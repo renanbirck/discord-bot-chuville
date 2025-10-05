f86b86aaa0fec478f4fbab49cb090bc525f8cc1c
Renan Birck Pinheiro <renan.birck.pinheiro@gmail.com>
2025-10-04 21:52:26 -0300

    feat(main): processamento das entradas 	estamos conseguindo ler as entradas!

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 rss-to-discord: bot do Discord para ler um feed RSS e converter em mensagens em um canal 
 
+### Dependências:
+    * feedreader
+    * requests
+
 ### Funcionamento
 Sempre que executado, o script `check_rss.py` lê o feed RSS especificado nas configurações e verifica se há algum post novo. Caso positivo, ele irá executar um comando para enviar para o Discord.
+
+Para servir de _cache_, os posts ficam em um banco de dados SQLite.
 
 ### Configurações
 No arquivo `config.py`:
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def main():
     logging.info("Iniciando a leitura do feed.")
     feed = feedparser.parse(config.RSS_URL)
     logging.info(f"O título do feed é {feed.feed.title}.")
+    for (entry_number, entry) in enumerate(feed.entries):
+        logging.info(f"A entrada {entry_number} tem o título {entry.title} e foi publicada em {entry.published}.")
 
 if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%Y/%m/%d %I:%M:%S %p', level=logging.INFO)
