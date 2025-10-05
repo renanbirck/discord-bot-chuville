0bcb63320164c0e076b2fbcd0368cc738356a270
Renan Birck Pinheiro <renan.birck.pinheiro@gmail.com>
2025-10-04 23:55:04 -0300

    feat(database, main): estamos conseguindo ler o RSS e adicionar as linhas no BD 	... agora é "só" fazer a integração com o Discord

--- database.py
+++ database.py
@@ -1,9 +1,33 @@
 import sqlite3
+from datetime import datetime 
+from time import mktime 
+import logging 
+
 DB_FILE_NAME='feed_contents.db'
 
 class Database:
     def __init__(self):
+       logging.info(f"Construindo o objeto de acesso ao BD...")
        self.connection = sqlite3.connect(DB_FILE_NAME)
        self.cursor = self.connection.cursor() 
+       self.cursor.execute("CREATE TABLE IF NOT EXISTS RSS_Entries(\
+                            entry_id INTEGER PRIMARY KEY,\
+                            entry_title VARCHAR(500) NOT NULL,\
+                            entry_publication_date VARCHAR(20) UNIQUE NOT NULL,\
+                            entry_summary VARCHAR(5000),\
+                            entry_link VARCHAR(500)\
+                            )")
+       logging.info(f"Sucesso! Podemos adicionar as entradas ao BD.")
 
-       self.cursor.execute("CREATE TABLE IF NOT EXISTS RSS_Entries(entry_id INTEGER, entry_title VARCHAR(500), entry_publication_date VARCHAR(20), entry_summary VARCHAR(5000)) ")
+    def put_entry(self, entry):
+
+        timestamp = datetime.fromtimestamp(mktime(entry.published_parsed))
+        logging.info(f"Adicionando entrada {entry.title} ao BD.")
+        self.cursor.execute("INSERT INTO RSS_ENTRIES(entry_title, entry_publication_date, entry_summary, entry_link)\
+                            VALUES(?, ?, ?, ?)", (entry.title, timestamp, entry.summary, entry.link))
+        self.connection.commit()
+
+    def __del__(self):
+        self.connection.commit()
+        self.connection.close()
+        logging.info(f"Destruíndo o objeto de acesso ao BD.")
--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import config 
 import feedparser
 import logging 
+import database 
 
 def main():
+
+    db = database.Database() 
+
     logging.info("Iniciando a leitura do feed.")
     feed = feedparser.parse(config.RSS_URL)
     logging.info(f"O título do feed é {feed.feed.title}.")
 
     for (entry_number, entry) in enumerate(feed.entries):
-        logging.info(f"A entrada {entry_number} tem o título {entry.title} e foi publicada em {entry.published}. Seu conteúdo é {entry.summary}. Mais informações em {entry.link}.")
+        logging.info(f"A entrada {entry_number} tem o título {entry.title} e foi publicada em {entry.published_parsed}. Seu conteúdo é {entry.summary}. Mais informações em {entry.link}.")
+        try:       
+            db.put_entry(entry)
+        except:
+            logging.info(f"A entrada da data {entry.published} já existe (não há nada de errado nisso, mas convém verificar).")
 
 if __name__ == "__main__":
-    logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%Y/%m/%d %I:%M:%S %p', level=logging.INFO)
+    logging.basicConfig(format='%(asctime)s %(funcName)s %(message)s', 
+                        datefmt='%Y/%m/%d %I:%M:%S %p', 
+                        level=logging.INFO)
     main()
